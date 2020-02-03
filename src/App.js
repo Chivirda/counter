7b31c3cd48b6c94dd64affad49b424bce8d1633e
Serg Ch <chivirdos@gmail.com>
2020-02-03 21:54:17 +0300

    92 урок готов.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import classes from './App.module.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    counter: 0
+  }
+
+  updateCounter(value) {
+    this.setState({
+      counter: this.state.counter + value
+    })
+  }
+
+  render() {
+    return (
+      <div className={classes.App}>
+        <h1>Counter: {this.state.counter}</h1>
+        <hr/>
+
+        <div className={classes.buttonWrap}>
+          <button 
+            className={classes.subtract}
+            onClick={() => this.updateCounter(-1)}
+          >&laquo;</button>
+          <button 
+            className={classes.append}
+            onClick={() => this.updateCounter(1)}
+          >&raquo;</button>
+        </div>
+
+      </div>
+    )
+  }
 }
-
-export default App;
